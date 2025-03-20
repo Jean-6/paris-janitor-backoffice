b0a058e94d2dcc75503fed153cdd014ad0537e3a
Jean <oka.jeanjaures@gmail.com>
2025-03-20 12:26:49 +0100

    feat: create and declare dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {DashboardComponent} from "./features/dashboard/dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  //Others root
+  {
+    path : 'login',
+    component:LoginComponent},
+  {
+    path : 'dashboard',
+    component:DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
