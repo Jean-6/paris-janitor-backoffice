bdb3227a86d20796cfa76f6d50d279029ddb406a
Jean <oka.jeanjaures@gmail.com>
2025-03-20 14:12:42 +0100

    feat: implement DTOs for connexion and registration

---
+++ src/app/_dto/authResponse.ts
@@ -0,0 +1,8 @@
+export class AuthResponse {
+  private id?: number; // TypeScript utilise 'number' pour les entiers
+  private email?: string; // Type 'string' pour les chaînes de caractères
+  private roles?: string[]; // Un tableau de chaînes pour représenter les rôles
+  private accessToken?: string; // Jeton d'accès
+  private refreshToken?: string; // Jeton de rafraîchissement
+  private tokenType?: string; // Type de jeton
+}
---
+++ src/app/_dto/registerRequest.ts
@@ -0,0 +1,8 @@
+export class RegisterRequest {
+
+  private firstname?:string;
+  private lastname?:string;
+  private email?:string;
+  private password?:string;
+  private role?:string;
+}
