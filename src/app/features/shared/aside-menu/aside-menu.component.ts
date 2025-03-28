cb1c0f15f85ad7f91863fb8cf66f22b11305ea97
Jean <oka.jeanjaures@gmail.com>
2025-03-28 15:25:13 +0100

    feat(ui): implement aside menu layout

---
+++ src/app/features/shared/aside-menu/aside-menu.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-aside-menu',
+  templateUrl: './aside-menu.component.html',
+  styleUrls: ['./aside-menu.component.css']
+})
+export class AsideMenuComponent implements OnInit{
+
+  ngOnInit() {
+  }
+}
