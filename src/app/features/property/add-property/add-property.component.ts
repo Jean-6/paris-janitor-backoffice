1ff6dd09c0ce668c4c4dab49b1631666e199bd79
Jean <oka.jeanjaures@gmail.com>
2025-04-24 23:47:07 +0200

    refactor: redefine to make central component

--- src/app/features/property/add-property/add-property.component.ts
+++ src/app/features/property/add-property/add-property.component.ts
@@ -1,61 +1,24 @@
-import {Component, inject, OnDestroy, OnInit} from '@angular/core';
-import {AuthService} from "../../../_services/auth.service";
-import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Component,  OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
-  styleUrls: ['./add-property.component.css']
+  styleUrls: ['./add-property.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddPropertyComponent implements OnInit,OnDestroy{
 
 
-  authService=inject(AuthService);
-  currentStep: number=2;
-  steps!: FormGroup[];
 
-
-  constructor(private formBuilder: FormBuilder, private router: Router) {
-
-    this.steps=[
-      this.formBuilder.group({
-        type:new FormControl('',Validators.required),
-      }),
-      this.formBuilder.group({
-        address:new FormControl('',Validators.required),
-      }),
-      this.formBuilder.group({
-        area:[],
-        nbOfPieces:[],
-        nbOfWaterroom:[],
-        nbOfBathroom:[],
-        isFurnished:[]
-
-      }),
-      this.formBuilder.group({
-
-      })
-    ]
+  constructor(private router: Router) {
   }
-
-
   ngOnDestroy(): void {
   }
 
   ngOnInit(): void {
-  }
-
-  previousStep(){
-    if(this.currentStep > 0) this.currentStep--;
-  }
-
-  nextStep(){
-    if(this.steps[this.currentStep].valid) this.currentStep++
-  }
-
-  submitMultiStepForm(){
-
+    this.router.navigate(['/characteristics']);
   }
 
 }
