d6ecaeb6ecddfe0b6bca0f57aa14569499ca3cb0
Jean <oka.jeanjaures@gmail.com>
2025-06-25 22:35:44 +0200

    styles : code dashboard design

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../_services/auth.service";
+import {CommonModule} from "@angular/common";
+import {AsideMenuComponent} from "../shared/aside-menu/aside-menu.component";
+import {AppModule} from "../../app.module";
+import {NavbarTopComponent} from "../shared/navbar-top/navbar-top.component";
+import {LoadingComponent} from "../shared/loading/loading.component";
 
 @Component({
   selector: 'app-dashboard',
