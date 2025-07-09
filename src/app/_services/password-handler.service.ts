f67c20ff3b58744441d005fecaf74b185a62f0f0
Jean <oka.jeanjaures@gmail.com>
2025-07-10 00:50:48 +0200

    fix: recode of service methods and their response according api

--- src/app/_services/password-handler.service.ts
+++ src/app/_services/password-handler.service.ts
@@ -1,28 +1,53 @@
 import { Injectable } from '@angular/core';
 import {LoginResponse} from "../_dto/loginResponse";
-import {HttpClient} from "@angular/common/http";
-import {ResetPasswordRequestDto} from "../_dto/resetPasswordRequestDto";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {ResetPassRequest} from "../_dto/resetPassRequest";
+import {catchError, map, Observable, throwError} from "rxjs";
+import {ResponseWrapper} from "../_dto/responseWrapper";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordHandlerService {
 
-  private apiUrlForgotPassword ="http://localhost:9090/api/password/forgot";
-  private apiUrlResetPassword ="http://localhost:9090/api/password/reset";
+  private urlSendEmail_ ="http://localhost:9090/api/password/send-email";
+  private urlResetPass_ ="http://localhost:9090/api/password/reset";
   isLoading = false;
 
   constructor(private _httpClient: HttpClient) { }
 
-  sendResetEmail(email: string){
-    return this._httpClient.post<{authResponse : LoginResponse}>(`${this.apiUrlForgotPassword}`,
-      {email},
-      { withCredentials: true });
+  sendResetEmail(email: string): Observable<string>{
+
+    const params = new HttpParams().set('email',email);
+    return this._httpClient.post<ResponseWrapper<string>>(`${this.urlSendEmail_}`,null,
+      {
+        params,
+        headers: new HttpHeaders({
+          'Content-Type':'application/json'
+        })
+      }).pipe(
+        map(response => response.data),
+      catchError(error => {
+        console.error('Error when sending mail', error);
+        return throwError(()=> new Error('Error when sending mail'));
+      })
+    );
   }
 
-  resetPassword(resetPasswordRequest: ResetPasswordRequestDto){
-    return this._httpClient.post<{response : String}>(`${this.apiUrlResetPassword}`,
+  resetPassword(resetPasswordRequest: ResetPassRequest): Observable<string>{
+    return this._httpClient.post<ResponseWrapper<string>>(`${this.urlResetPass_}`,
       resetPasswordRequest,
-      { withCredentials: true });
+      {
+        headers: new HttpHeaders({
+          'Content-Type' : 'application/json'
+        })
+      }).pipe(
+        map(response => response.data),
+      catchError(error => {
+        console.error('Error when reset password', error);
+        return throwError(()=> new Error('Error when reset password'));
+
+      })
+    );
   }
 }
