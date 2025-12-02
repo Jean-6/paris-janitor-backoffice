f07aa1ba20e589e298ffb3c4e71783dc70c57308
Jean <oka.jeanjaures@gmail.com>
2025-12-02 11:37:06 +0100

    fix error: import provider for httpClientModule

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,23 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+import {provideRouter} from "@angular/router";
+import {routes} from "./app/app-routing.module";
+import {importProvidersFrom} from "@angular/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {bootstrapApplication} from "@angular/platform-browser";
+import {AppComponent} from "./app/app.component";
+import {MessageService} from "primeng/api";
+import {provideHttpClient} from "@angular/common/http";
+
+
+
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    MessageService,
+    importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient()
+  ]
+}).catch(err => console.error(err));
+
